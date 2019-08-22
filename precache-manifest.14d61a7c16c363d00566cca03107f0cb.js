6f03d5655402c4675e04d2890af58709ae82fc8f
pixeluga <anc@bigmir.net>
2019-08-22 13:10:43 +0300

    Updates

---
+++ precache-manifest.14d61a7c16c363d00566cca03107f0cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e3707fadfa56090532ad9825318dbc",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "c31dcdad65df773da21f",
+    "url": "/rick-morty/static/css/main.fd201ac9.chunk.css"
+  },
+  {
+    "revision": "00aad2d283c53c977865",
+    "url": "/rick-morty/static/js/2.16e253f4.chunk.js"
+  },
+  {
+    "revision": "c31dcdad65df773da21f",
+    "url": "/rick-morty/static/js/main.a30ea089.chunk.js"
+  },
+  {
+    "revision": "8126769c8f774099f795",
+    "url": "/rick-morty/static/js/runtime~main.d253e5e4.js"
+  },
+  {
+    "revision": "f2eb23553c3ac4a2ed6735f6ef80a6a6",
+    "url": "/rick-morty/static/media/loading.f2eb2355.gif"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/rick-morty/static/media/logo.75509f2e.png"
+  }
+]);
